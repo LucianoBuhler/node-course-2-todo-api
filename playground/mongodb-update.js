ea0573b705ce2b4855af367a10ab49e2529b9b8a
Luciano <luciano.buhler@navomi.com>
2018-09-10 18:47:25 -0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err){
+    return console.log('Unable to connect to MongoDB server:', err);
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b927e10c0a5da72c0923719')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b92808cfeb7b073938c0dd2')
+  }, {
+    $set: {
+      name: 'Buhleroid',
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+
+  // client.close();
+});
