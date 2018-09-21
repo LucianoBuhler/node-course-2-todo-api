b71c6ad2257ead1ab49a3273b3279f4a79be5452
Luciano <luciano.buhler@navomi.com>
2018-09-21 15:13:49 -0300

    Setup separated test and development envs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -157,11 +157,11 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(true);
-        expect(res.body.todo.completedAt).toBe('number');
+        expect(typeof res.body.todo.completedAt).toBe('number');
       })
       .end(done)
   });
-  
+
   it('should clear completeAt when todo is not completed', (done) => {
     var hexId = todos[1]._id.toHexString();
     var text = 'This is a Patch test 2';
