8a9f3205f625b2c41252ba2279b4a949db34df8e
Luciano <luciano.buhler@navomi.com>
2018-09-24 16:57:11 -0300

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,27 @@ UserSchema.methods.generateAuthToken = function (){
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, '123123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject()
+    // });
+    return Promise.reject(); // exactly the same functionality than last block
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
