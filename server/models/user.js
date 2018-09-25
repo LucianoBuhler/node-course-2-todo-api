3d3fd5e8d04cc1ae11de54d55f5add76a5314717
Luciano <luciano.buhler@navomi.com>
2018-09-25 19:25:14 -0300

    Add tests for GET /users/me and POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -83,7 +83,7 @@ UserSchema.pre('save', function(next) {
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
-        console.log(hash);
+        // console.log(hash);
         next();
       });
     });
